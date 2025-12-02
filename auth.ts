9f3adbd700d73d28ec141cf918c171b71775d473
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-12-02 12:18:12 +0800

    fix: error set up prisma with auth.js

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   session: { strategy: "jwt" },
   pages: {
