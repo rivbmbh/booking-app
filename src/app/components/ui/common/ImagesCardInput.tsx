bb8f59ac64b7bd2e0c358c13362e757653c5101d
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2026-01-02 20:50:39 +0800

    feat swap image card input

--- src/app/components/ui/common/ImagesCardInput.tsx
+++ src/app/components/ui/common/ImagesCardInput.tsx
@@ -1,34 +1,51 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 const ImagesCardInput = ({ image }: { image: string }) => {
+  const [imageList, setImageList] = useState<string[]>([
+    image,
+    "/hero3.jpg",
+    "/hero2.jpg",
+  ]);
+
+  function handleThumbnailClick(ClickedImage: string) {
+    const clickedIndex = imageList.indexOf(ClickedImage);
+    if (clickedIndex === -1) return;
+
+    setImageList((prevImageList) => {
+      const updatedImageList = [...prevImageList];
+      // swap primary image dengan image yang diklik
+      [updatedImageList[0], updatedImageList[clickedIndex]] = [
+        updatedImageList[clickedIndex],
+        updatedImageList[0],
+      ];
+
+      return updatedImageList;
+    });
+  }
   return (
     <div className="aspect-video relative w-full mb-2">
       <Image
-        src={image}
+        src={imageList[0]}
         alt="Room Image"
         width={200}
         height={100}
         className="object-cover w-full rounded-2xl aspect-video"
       />
       <div className="absolute left-3 bottom-3 object-cover flex gap-4 justify-evenly">
-        <div className="cursor-pointer">
-          <Image
-            src={image}
-            alt="Room Image"
-            width={50}
-            height={30}
-            className="object-cover w-full rounded-md border border-dashed border-gray-300 aspect-video hover:scale-110 transition-all duration-200 ease-in-out"
-          />
-        </div>
-        <div className=" cursor-pointer">
-          <Image
-            src={image}
-            alt="Room Image"
-            width={50}
-            height={30}
-            className="object-cover w-full rounded-md border border-dashed border-gray-300 aspect-video hover:scale-110 transition-all duration-200 ease-in-out"
-          />
-        </div>
+        {imageList.slice(1).map((thumbnail, index) => (
+          <div key={index} className="cursor-pointer">
+            <Image
+              src={thumbnail}
+              alt="Room Image"
+              width={50}
+              height={30}
+              onClick={() => handleThumbnailClick(thumbnail)}
+              className="object-cover w-full rounded-md border border-dashed border-gray-300 aspect-video hover:scale-110 transition-all duration-200 ease-in-out"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/app/components/ui/common/room/RoomDetailUser.tsx
+++ src/app/components/ui/common/room/RoomDetailUser.tsx
@@ -1,9 +1,9 @@
 import { getDisabledRoomById, getRoomDetailById } from "@/lib/data";
 import { formatCurrency } from "@/lib/utils";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import { IoCheckmark, IoPeopleOutline } from "react-icons/io5";
 import ReserveForm from "../ReserveForm";
+import ImagesCardInput from "../ImagesCardInput";
 
 const RoomDetailUser = async ({ roomId }: { roomId: string }) => {
   const [room, disabledDate] = await Promise.all([
@@ -14,15 +14,10 @@ const RoomDetailUser = async ({ roomId }: { roomId: string }) => {
   return (
     <div className="max-w-screen-2xl py-16 px-4 grid lg:grid-cols-12 gap-8 mx-auto">
       <div className="md:col-span-8">
-        <Image
-          src={room.image}
-          alt={room.name}
-          width={770}
-          height={430}
-          priority
-          className="w-full rounded-sm mb-8"
-        />
-        <h1 className="text-5xl font-bold text-gray-900 mb-8">{room.name}</h1>
+        <ImagesCardInput image={room.image} />
+        <h1 className="text-5xl font-bold text-gray-900 mb-8 pt-3">
+          {room.name}
+        </h1>
         <p>{room.description}</p>
         <h5 className="text-[20px] font-semibold py-1 mt-1">Amenities :</h5>
         <div className="grid md:grid-cols-3">
@@ -37,7 +32,7 @@ const RoomDetailUser = async ({ roomId }: { roomId: string }) => {
         </div>
       </div>
       <div className="md:col-span-4">
-        <div className="border-2 border-gray-200 border-dashed px-3 py-5 bg-old-paper rounded-md">
+        <div className="border-2 border-gray-200 border-dashed px-3 py-5 bg-old-paper rounded-none">
           <div className="flex items-center justify-between mb-8">
             <div className="flex items-center space-x-2">
               <IoPeopleOutline className="size-4" />
