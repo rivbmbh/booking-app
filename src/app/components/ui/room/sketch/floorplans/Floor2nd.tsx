bcbdc67ada3ba456432ce57c27a27fd5d406588c
rivbmbh <rivaldoangelobomboah@gmail.com>
2026-02-10 14:30:52 +0800

    feat: handle click room

--- src/app/components/ui/room/sketch/floorplans/Floor2nd.tsx
+++ src/app/components/ui/room/sketch/floorplans/Floor2nd.tsx
@@ -3,24 +3,45 @@
 import { useEffect, useState } from 'react';
 
 export default function FloorPlan2nd() {
-  const [svg, setSvg] = useState('');
+  const [svg,  setSvg] = useState('');
 
   useEffect(() => {
-    fetch('/floorplans/2ndfloor.min.svg')
+    fetch('/floorplans/lantai_2.svg')
       .then(res => res.text())
       .then(setSvg);
   }, []);
 
   useEffect(() => {
-    const room = document.getElementById('room-101');
-    console.info(room)
-    room?.addEventListener('click', () => {
-      alert('Room 101 clicked');
-    });
+    if (!svg) return;
+
+    const container = document.getElementById('svg-container');
+    if (!container) return;
+
+    const handleClick = (e) => {
+      const target = e.target;
+
+      // Naik ke parent <g id="room-xxx">
+      const room = target.closest('g[id^="room-"]');// cari tag <g> dengan id diawali "room-"
+
+      if (!room) return; // jika tidak ada, keluar
+
+      alert(`Klik kamar ${room.id.replace('room-', '')}`);
+    };
+
+    container.addEventListener('click', handleClick);
+
+    return () => {
+      container.removeEventListener('click', handleClick);
+    };
   }, [svg]);
+
+
+
+
 
   return (
     <div
+      id="svg-container"
       dangerouslySetInnerHTML={{ __html: svg }}
       className="w-full h-[600px]"
     />
