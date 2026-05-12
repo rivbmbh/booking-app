639d361f2146182ad148444d0d5d549c1fdd9cc8
rivbmbh <rivaldoangelobomboah@gmail.com>
2026-05-12 15:04:43 +0800

    fix: props roomdetail sidebar

--- src/app/components/ui/card/Card.tsx
+++ src/app/components/ui/card/Card.tsx
@@ -3,7 +3,6 @@ import { RoomTypeProps } from "@/types/room";
 import Image from "next/image";
 import Link from "next/link";
 import { IoPeopleOutline } from "react-icons/io5";
-import ModalChooseBed from "../common/room/form/ModalChooseBed";
 
 const Card = ({ roomType }: { roomType: RoomTypeProps }) => {
   if (!roomType) return null;
