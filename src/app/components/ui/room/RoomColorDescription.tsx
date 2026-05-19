25ce4788d71f2fae7e029fcc57158e030352c693
rivbmbh <rivaldoangelobomboah@gmail.com>
2026-05-19 14:58:19 +0800

    fix: room color description

--- src/app/components/ui/room/RoomColorDescription.tsx
+++ src/app/components/ui/room/RoomColorDescription.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const RoomColorDescription = () => {
   return (
     <div className=''>
-        <ul className='grid grid-cols-2 gap-8 justify-between text-sm'>
+        <ul className='grid grid-cols-2 gap-5 justify-between text-sm'>
             <li className='space-y-2.5'>
               <div className='w-5 h-5 bg-white mx-auto border border-gray-500'></div>
               <p className='text-center'>Available</p>
@@ -21,17 +21,23 @@ const RoomColorDescription = () => {
               <div className='w-5 h-5 bg-[#0459e0] mx-auto border border-gray-500'></div>
               <p className='text-center'>Selected</p>
             </li>
-             <li className='space-y-2.5'>
+            <li className='space-y-2.5'>
               <div className='flex justify-center items-center'>
-              <Image src="/floorplans/singlebed.png" alt="Single Bed" width={26} height={26} />
+              <Image src="/bedtype/single.png" alt="Single Bed" width={14} height={16} />
               </div>
               <p className='text-center'>Single Bed</p>
             </li>
-             <li className='space-y-2.5'>
+            <li className='space-y-2.5'>
               <div className='flex justify-center items-center'>
-              <Image src="/floorplans/doublebed.png" alt="Double Bed" width={30} height={30} />
+              <Image src="/bedtype/king.png" alt="King Bed" width={26} height={26} />
               </div>
-              <p className='text-center'>Double Bed</p>
+              <p className='text-center'>King/Queen Bed</p>
+            </li>
+            <li className='space-y-2.5'>
+              <div className='flex justify-center items-center'>
+              <Image src="/bedtype/twin.png" alt="Double Bed" width={30} height={30} />
+              </div>
+              <p className='text-center'>Double/Twin Bed</p>
             </li>
         </ul>
     </div>
